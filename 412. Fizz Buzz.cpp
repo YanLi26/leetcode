dbdc853071f947d1335c1a6c5998a204456865aa
YanLi26 <yali2241@colorado.edu>
2016-11-01 12:17:39 -0600

    Add some previous items

---
+++ 412. Fizz Buzz.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> fb;
+        for(int i = 0; i < n; i++){
+            if((i+1)%3 == 0 && (i+1) % 5 == 0)
+                fb.push_back("FizzBuzz");
+            else if((i+1)%3 == 0)
+                fb.push_back("Fizz");
+            else if((i+1)%5 == 0)
+                fb.push_back("Buzz");
+            else fb.push_back(to_string(i+1));
+        }
+        return fb;
+    }
+};
