775a433befc601df9c368a5111c1ba659b3e885a
YanLi26 <yali2241@colorado.edu>
2017-02-01 11:55:56 -0700

    leetcode

--- 100. Same Tree.cpp
+++ 100. Same Tree.cpp
@@ -7,13 +7,13 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-/* https://helloacm.com/coding-exercise-check-same-tree-c-online-judge/*/
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if ((p == NULL) && (q == NULL)) return true;
-        if (p == NULL) return false;
-        if (q == NULL) return false;
-        return (p->val == q->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        if(p == NULL && q == NULL) return true;
+        else if (p == NULL) return false;
+        else if (q == NULL) return false;
+        else if(p->val != q->val) return false;
+        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
     }
 };
--- 409. Longest Palindrome.cpp
+++ 409. Longest Palindrome.cpp
@@ -1,16 +1,14 @@
-//it can use set or XOR http://www.cnblogs.com/grandyang/p/5931874.html
-
 class Solution {
 public:
     int longestPalindrome(string s) {
-        int res = 0, tmp = 0;
-        unordered_map<char, int> map;
-        for(char c: s) ++map[c];
-        for(unordered_map<char, int>::iterator it = map.begin(); it != map.end(); it++){
-            res += it->second/2*2;
-            if(it->second % 2 == 1) tmp = 1;
+        int res = 0, flag = 0;
+        unordered_map<char,int> pali;
+        for(char a: s) ++pali[a];
+        for(unordered_map<char, int>::iterator it = pali.begin(); it != pali.end(); it++){
+            res += it -> second/2*2;
+            if(it -> second %2 == 1) flag = 1;
         }
-        if(tmp == 1) return ++res;
+        if(flag == 1) return ++res;
         else return res;
     }
 };
