a5bd1cd9b0832fde72bbd78fb255ec99e8237085
YanLi26 <yali2241@colorado.edu>
2017-02-12 17:08:15 -0700

    leetcode

--- 118. Pascal's Triangle.cpp
+++ 118. Pascal's Triangle.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> res;
-        for(int i = 1; i <= numRows; i++){
-            vector<int> tmp;
-            for(int j = 0; j < i; j++){
-                if(j == 0 || j == i-1) tmp.push_back(1);
-                else{
-                    tmp.push_back(res[i-2][j-1] + res[i-2][j]);
-                }
+        if(numRows <= 0) return res;
+        res.assign(numRows,vector<int>(1));
+        res[0][0] = 1;
+        for(int i = 1; i < numRows; i++){
+            res[i][0]=1;
+            for (int j = 1; j < i; ++j) {
+                res[i].push_back(res[i-1][j-1] + res[i-1][j]);
             }
-            res.push_back(tmp);
+            res[i].push_back(1);
         }
         return res;
     }
