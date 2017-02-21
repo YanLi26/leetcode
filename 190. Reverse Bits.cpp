dad4c3f09e65c05b178eabc55ec70ca5ea6e6dc8
YanLi26 <yali2241@colorado.edu>
2017-02-21 15:45:12 -0700

    leetcode

--- 190. Reverse Bits.cpp
+++ 190. Reverse Bits.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
         uint32_t res = 0;
-        for(int i=0; i<32; i++){
-            if(n & 1 == 1) res = (res << 1) + 1;
-            else res = res << 1;
-            n = n >> 1;
+        for(int i = 0; i < 32; i++){
+            res = res<<1;
+            if(n & 1 == 1) res += 1;
+            n = n>>1;
         }
         return res;
     }
--- 400. Nth Digit.cpp
+++ 400. Nth Digit.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
     int findNthDigit(int n) {
-        long long begin = 1, nums = 9, digit = 1;
-        while(n > begin * nums * digit){
-            n -= begin * nums * digit;
-            begin *= 10;
+        int digit = 1, res = 0, num = 0;
+        vector<int> nums;
+        while(n > 9*pow(10, digit-1)*digit){
+            n -= 9*pow(10, digit-1)*digit;
             digit++;
         }
-        begin += (n-1)/digit;
-        string s = to_string(begin);
-        return s[(n - 1) % digit] - '0';
+        num = pow(10, digit-1) + n/digit - 1;
+        if(n%digit == 0) res = num%10;
+        else{
+            num++;
+            while(num){
+                nums.push_back(num%10);
+                num /= 10;
+            }
+            reverse(nums.begin(),nums.end());
+            res = nums[n%digit - 1];
+        }
+        
+        return res;
     }
 };
